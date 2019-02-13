2be33839a59ed4c0a6f45ecadc49736af824f93a
yunxi.zhang <yunxi.zhang@accenture.com>
2019-02-13 16:06:09 +0000

    update stage 5 file

--- stage5_putChannelConfigTxToContainer.sh
+++ stage5_putChannelConfigTxToContainer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo -e "\x1b[33mCreate A Channel And Join The Channel \x1b[0m "
+echo -e "\x1b[33mCreate A Channel \x1b[0m "
 docker exec -it cli peer channel create -o orderer.yunxi.com:7050 -c $CHANNELNAME -f ./channel-artifacts/channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/yunxi.com/orderers/orderer.yunxi.com/msp/tlscacerts/tlsca.yunxi.com-cert.pem
+echo -e "\x1b[33mA Peer Joins A Channel \x1b[0m "
 docker exec -it cli peer channel join -b $CHANNELNAME.block

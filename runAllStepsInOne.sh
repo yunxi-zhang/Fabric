cd4c2f6e597443df2590c156a9f3c30cb8cb7701
yunxi.zhang <yunxi.zhang@accenture.com>
2019-02-13 12:13:21 +0000

    update the all in one file

--- runAllStepsInOne.sh
+++ runAllStepsInOne.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# The steps 1-3 will remove all docker iamges in a machine
+# comment them out if this is not the right intention to do
+
+# Step 1: stop all current docker containers including those not in this project
+docker stop -f $(docker ps -aq)
+# Step 2: remove all docker containers
+docker rm -f $(docker ps -aq)
+# Step 3: remove all docker images
+docker rmi -f $(docker images -aq)
+
+# run all other shell files for this project
 ./step1_downloadbin.sh
 ./step2_generateKey.sh
 ./step3_generateConfigTx.sh

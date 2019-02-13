8102c14192e2d7cc1e946fdc2ad4c7ba5a172483
yunxi.zhang <yunxi.zhang@accenture.com>
2019-02-13 11:12:46 +0000

    update step3 shell

--- step3_generateConfigTx.sh
+++ step3_generateConfigTx.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 mkdir ./channel-artifacts
+# generate the genesis block
 ./bin/configtxgen -configPath ./ -profile TwoOrgsOrdererGenesis -channelID seller-buyer-channel -outputBlock ./channel-artifacts/genesis.block 
+# generate channel transaction artifact
+./bin/configtxgen -configPath ./ -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID channel1

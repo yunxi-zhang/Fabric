f8285aa482054909af8d202f2f012c5170387cee
yunxi.zhang <yunxi.zhang@accenture.com>
2019-02-14 13:50:25 +0000

    update stage3

--- stage3_generateConfigTx.sh
+++ stage3_generateConfigTx.sh
@@ -8,8 +8,8 @@ mkdir ./channel-artifacts
 # generate the genesis block
 ./bin/configtxgen -configPath ./ -profile TwoOrgsOrdererGenesis -channelID seller-buyer-channel -outputBlock ./channel-artifacts/genesis.block 
 # generate channel transaction artifact
-./bin/configtxgen -configPath ./ -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNELNAME
+./bin/configtxgen -configPath ./ -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
 # generate anchor peer for seller
-./bin/configtxgen -configPath ./ -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/sellerMSPanchors.tx -channelID $CHANNELNAME -asOrg SellerMSP
+./bin/configtxgen -configPath ./ -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/sellerMSPanchors.tx -channelID $CHANNEL_NAME -asOrg SellerMSP
 # generate anchor peer for buyer
-./bin/configtxgen -configPath ./ -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/buyerMSPanchors.tx -channelID $CHANNELNAME -asOrg BuyerMSP
+./bin/configtxgen -configPath ./ -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/buyerMSPanchors.tx -channelID $CHANNEL_NAME -asOrg BuyerMSP

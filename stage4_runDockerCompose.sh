28fbf2bf80bc856a7acc01f034cc61b51bb19c43
yunxi.zhang <yunxi.zhang@accenture.com>
2019-02-14 14:45:33 +0000

    update stage 4

--- stage4_runDockerCompose.sh
+++ stage4_runDockerCompose.sh
@@ -4,4 +4,4 @@
 source ./common.sh
 
 stepInfo "Run Docker Containers By Using The Docker Compose File"
-docker-compose -f docker-compose-cli.yaml up -d
+docker-compose -f docker-compose-cli.yaml up -d 

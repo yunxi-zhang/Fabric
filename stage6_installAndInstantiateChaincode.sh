f475e4361a5301ec8d2274fd1963b46b88b15964
yunxi.zhang <yunxi.zhang@accenture.com>
2019-02-15 10:43:53 +0000

    update stage6 file

--- stage6_installAndInstantiateChaincode.sh
+++ stage6_installAndInstantiateChaincode.sh
@@ -15,13 +15,13 @@ stepInfo "The received chaincode name is: $CHAINCODE_NAME"
 
 # type in a language for build
 stepInfo "Please type in a build language, can only be (1) golang (2) node and (3) java"
-read BUILDLANGUAGE
-stepInfo "The received build language is: $BUILDLANGUAGE"
+read BUILD_LANGUAGE
+stepInfo "The received build language is: $BUILD_LANGUAGE"
 
 stepInfo "Install Chaincode On Peer0 Of Seller"
-docker exec -it cli peer chaincode install -n $CHAINCODE_NAME -v $CHAINCODE_VERSION -l $BUILDLANGUAGE -p /opt/gopath/src/github.com/chaincode/buy_sell/$BUILDLANGUAGE/
+docker exec -it cli peer chaincode install -n $CHAINCODE_NAME -v $CHAINCODE_VERSION -l $BUILD_LANGUAGE -p /opt/gopath/src/github.com/chaincode/buy_sell/$BUILD_LANGUAGE/
 stepInfo "Install Chaincode On Peer0 Of Buyer"
-docker exec -it cli peer chaincode install -n $CHAINCODE_NAME -v $CHAINCODE_VERSION -l $BUILDLANGUAGE -p /opt/gopath/src/github.com/chaincode/buy_sell/$BUILDLANGUAGE/
+docker exec -it cli peer chaincode install -n $CHAINCODE_NAME -v $CHAINCODE_VERSION -l $BUILD_LANGUAGE -p /opt/gopath/src/github.com/chaincode/buy_sell/$BUILD_LANGUAGE/
 
 stepInfo "Instantiate Chaincode"
-docker exec -it cli peer chaincode instantiate -o orderer.yunxi.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/yunxi.com/orderers/orderer.yunxi.com/msp/tlscacerts/tlsca.yunxi.com-cert.pem -C $CHANNEL_NAME -n $CHAINCODE_NAME -l $BUILDLANGUAGE -v $CHAINCODE_VERSION -c '{"Args":["init"]}' -P "AND ('SellerMSP.peer','BuyerMSP.peer')"
+docker exec -it cli peer chaincode instantiate -o orderer.yunxi.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/yunxi.com/orderers/orderer.yunxi.com/msp/tlscacerts/tlsca.yunxi.com-cert.pem -C $CHANNEL_NAME -n $CHAINCODE_NAME -l $BUILD_LANGUAGE -v $CHAINCODE_VERSION -c '{"Args":["init"]}' -P "AND ('SellerMSP.peer','BuyerMSP.peer')"

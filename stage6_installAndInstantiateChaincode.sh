3b4c128966db3e610d88455d3cb5147ca536db2d
yunxi.zhang <yunxi.zhang@accenture.com>
2019-02-15 11:21:12 +0000

    use parameter for chaincode path

--- stage6_installAndInstantiateChaincode.sh
+++ stage6_installAndInstantiateChaincode.sh
@@ -18,10 +18,13 @@ stepInfo "Please type in a build language, can only be (1) golang (2) node and (
 read BUILD_LANGUAGE
 stepInfo "The received build language is: $BUILD_LANGUAGE"
 
+CHAINCODE_PATH="/opt/gopath/src/github.com/chaincode/buy_sell"
+stepInfo "Chaincode file is in the path: $CHAINCODE_PATH"
+
 stepInfo "Install Chaincode On Peer0 Of Seller"
-docker exec -it cli peer chaincode install -n $CHAINCODE_NAME -v $CHAINCODE_VERSION -l $BUILD_LANGUAGE -p /opt/gopath/src/github.com/chaincode/buy_sell/$BUILD_LANGUAGE/
+docker exec -it cli peer chaincode install -n $CHAINCODE_NAME -v $CHAINCODE_VERSION -l $BUILD_LANGUAGE -p $CHAINCODE_PATH/$BUILD_LANGUAGE/
 stepInfo "Install Chaincode On Peer0 Of Buyer"
-docker exec -it cli peer chaincode install -n $CHAINCODE_NAME -v $CHAINCODE_VERSION -l $BUILD_LANGUAGE -p /opt/gopath/src/github.com/chaincode/buy_sell/$BUILD_LANGUAGE/
+docker exec -it cli peer chaincode install -n $CHAINCODE_NAME -v $CHAINCODE_VERSION -l $BUILD_LANGUAGE -p $CHAINCODE_PATH/$BUILD_LANGUAGE/
 
 stepInfo "Instantiate Chaincode"
 docker exec -it cli peer chaincode instantiate -o orderer.yunxi.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/yunxi.com/orderers/orderer.yunxi.com/msp/tlscacerts/tlsca.yunxi.com-cert.pem -C $CHANNEL_NAME -n $CHAINCODE_NAME -l $BUILD_LANGUAGE -v $CHAINCODE_VERSION -c '{"Args":["init"]}' -P "AND ('SellerMSP.peer','BuyerMSP.peer')"

bc799d028d2e3266e0985b9fd23eaa38d210c975
yunxi.zhang <yunxi.zhang@accenture.com>
2019-02-18 14:35:47 +0000

    update chaincode

--- chaincode/buy_sell/node/chaincode_buy_sell.js
+++ chaincode/buy_sell/node/chaincode_buy_sell.js
@@ -5,40 +5,35 @@ const util = require('util');
 
 const Chaincode = class {
 
-    async Init(stub) {
-        // let args = stub.getFunctionAndParameters().params;
-        // console.log(args);
-
-        // // save the initial states
-        // return stub.putState('dummykey', Buffer.from('dummyValue'))
-        //             .then(() => {
-        //                 console.info('Chaincode instantiation is successful');
-        //                 return shim.success();
-        //             }), () => {
-        //                 return shim.error();
-        //             };
+    Init(stub) {
         console.log('========= Chaincode Initialised =========');
-        const ret = stub.getFunctionAndParameters();
-        console.log(ret);
-        return shim.success();
+        return stub.putState('dummyKey', Buffer.from('dummyValue'))
+            .then(() => {
+                console.info('Chaincode instantiation is successful');
+                return shim.success();
+            }, () => {
+                return shim.error();
+            })
     }
 
-    async Invoke(stub) {
+    Invoke(stub) {
+        console.log('========= Chaincode Invoked =========');
         console.info('Transaction ID: ' + stub.getTxID());
         console.info(util.format('Args: %j', stub.getArgs()));
 
         let ret = stub.getFunctionAndParameters();
         console.info('Calling function: ' + ret.fcn);
+
         return stub.getState('dummyKey')
-            .then((value) => {
-                if (value.toString === 'dummyValue') {
-                    console.info(util.format('successfully retrieved value "%j" for the key "dummyKey"', value));
-                    return shim.success();
+        .then((value) => {
+                if (value.toString() === 'dummyValue') {
+                        console.info(util.format('successfully retrieved value "%j" for the key "dummyKey"', value ));
+                        return shim.success();
                 } else {
-                    console.error('Failed to retrieve dummyKey or the retrieved value is not expected: ' + value);
-                    return shim.error();
+                        console.error('Failed to retrieve dummyKey or the retrieved value is not expected: ' + value);
+                        return shim.error();
                 }
-            })
+        });
     }
 };
 

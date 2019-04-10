23c16c05ad79950b76afbf2b6816e0a05fb1da77
yunxi-zhang <yunxi.zhang@hotmail.com>
2019-04-10 23:43:01 +0100

    update get apis

---
+++ myapp/server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var connectFabricNetwork = require("./connectFabricNetwork")
+var app = express(),
+    port = process.env.PORT || 3000;
+
+app.listen(port, ()=> {
+    console.log("server is running on port 3000");
+})
+
+app.get("/getSellerBalance", async (req, res, next) => {
+    let balance = await connectFabricNetwork.getSellerBalance();
+    res.json(JSON.parse(balance));
+});
+
+app.get("/getBuyerBalance", async (req, res, next) => {
+    let balance = await connectFabricNetwork.getBuyerBalance();
+    res.json(JSON.parse(balance));
+})

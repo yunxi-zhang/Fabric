bf275ba343cc397c65c18062a26f2056668d43d7
yunxi-zhang <yunxi.zhang@hotmail.com>
2019-04-10 22:17:58 +0100

    add sleep after updating the ledger

--- stage6_installAndInstantiateChaincode.sh
+++ stage6_installAndInstantiateChaincode.sh
@@ -81,6 +81,9 @@ INOVKE_CHAINCODE_PARAMETERS='{"Args":["update","sellerBalance","150"]}'
 docker exec -it cli \
 peer chaincode invoke -o orderer.yunxi.com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/yunxi.com/orderers/orderer.yunxi.com/msp/tlscacerts/tlsca.yunxi.com-cert.pem -C $CHANNEL_NAME -n $CHAINCODE_NAME --peerAddresses peer0.seller.yunxi.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/seller.yunxi.com/peers/peer0.seller.yunxi.com/tls/ca.crt -c $INOVKE_CHAINCODE_PARAMETERS
 
+stepInfo "Sleeping for 5s to wait for data update to complete"
+sleep 5
+
 stepInfo "Test Query Function for Getting Seller's Balance again"
 TEST_CHAINCODE_PARAMETERS='{"Args":["get","sellerBalance"]}'
 docker exec -it cli \

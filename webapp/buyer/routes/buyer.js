d9abd66a8e6481ea01fe1ae4a4cabf956f5ad032
yunxi.zhang <yunxi.zhang@accenture.com>
2020-06-27 20:22:47 +0100

    add cors support for buyer and seller

--- webapp/buyer/routes/buyer.js
+++ webapp/buyer/routes/buyer.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var cors = require('cors');
 var router = express.Router();
 var money = require('../money/money');
 router.use(express.json());
+router.use(cors());
 
 router.get("/balance", async (req, res) => {
     let balance = await money.getBuyerBalance();

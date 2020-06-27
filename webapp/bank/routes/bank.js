e4ee1f10f57cc0f67e0cabec680ceb0f6d1b2c75
yunxi.zhang <yunxi.zhang@accenture.com>
2020-06-27 20:18:32 +0100

    add cors support

--- webapp/bank/routes/bank.js
+++ webapp/bank/routes/bank.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var cors = require('cors');
 var router = express.Router();
 var money = require('../money/money');
 router.use(express.json());
+router.use(cors());
 
 router.get("/sellerBalance", async (req, res, next) => {
     let balance = await money.getSellerBalance();
